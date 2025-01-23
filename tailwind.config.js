cd0ed841a85683e88a2a07e1728956378e57598a
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-01-23 17:51:38 -0500

    Get a working sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ['class'],
-    content: ['./src/**/*.{ts,tsx}'],
+    content: ['./src/**/*.{ts,tsx,html}'],
     theme: {
         extend: {
             colors: {
@@ -38,10 +38,22 @@ export default {
                     DEFAULT: 'hsl(var(--card))',
                     foreground: 'hsl(var(--card-foreground))',
                 },
+                sidebar: {
+                    DEFAULT: 'hsl(var(--sidebar-background))',
+                    foreground: 'hsl(var(--sidebar-foreground))',
+                    primary: 'hsl(var(--sidebar-primary))',
+                    'primary-foreground':
+                        'hsl(var(--sidebar-primary-foreground))',
+                    accent: 'hsl(var(--sidebar-accent))',
+                    'accent-foreground':
+                        'hsl(var(--sidebar-accent-foreground))',
+                    border: 'hsl(var(--sidebar-border))',
+                    ring: 'hsl(var(--sidebar-ring))',
+                },
             },
             borderRadius: {
-                lg: `var(--radius)`,
-                md: `calc(var(--radius) - 2px)`,
+                lg: '`var(--radius)`',
+                md: '`calc(var(--radius) - 2px)`',
                 sm: 'calc(var(--radius) - 4px)',
             },
         },
