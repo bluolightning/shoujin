6dd34de57dd0d4c696b8a841acd2be50fd4c502d
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-01-31 13:54:55 -0500

    Get a pie chart with site name on outside labels

--- src/entrypoints/mainPage/Dopcan.tsx
+++ src/entrypoints/mainPage/Dopcan.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { DonutTestChart } from '@/components/DonutTestChart';
+import { TestChart } from '@/components/TestChart';
 
 const Dopcan = () => {
     return (
         <div>
             <h1>
-                Hello from the <strong>Dopcan</strong> page of the app! (lol)
+                Hello from the <strong>Dopcan</strong> page of the app!
             </h1>
+
+            <DonutTestChart />
+            <TestChart />
         </div>
     );
 };
