e4af643417f1564f9a2577387a91cc739ca55f97
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-02-02 17:48:18 -0500

    Remove the duplicate pie chart since it takes longer to update both

--- src/entrypoints/mainPage/Dopcan.tsx
+++ src/entrypoints/mainPage/Dopcan.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { DonutTestChart } from '@/components/DonutTestChart';
 
 const Dopcan = () => {
     return (
-        <div>
+        <>
             <h1>
                 Hello from the <strong>Dopcan</strong> page of the app!
             </h1>
-
-            <DonutTestChart />
-        </div>
+        </>
     );
 };
 
