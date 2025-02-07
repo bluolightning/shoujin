0f7ab0547850f8384e3c2e7ff55db6d909cd2fe3
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-02-07 16:43:51 -0500

    Remove unnecessary code and improve formatting

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,31 +1,6 @@
 import { StorageManager } from '@/modules/storage';
 
 export default defineBackground(() => {
-    async function getCurrentTabUrl() {
-        try {
-            const tabs = await browser.tabs.query({
-                active: true,
-                currentWindow: true,
-            });
-            if (tabs && tabs[0] && tabs[0].url) {
-                return tabs[0].url;
-            }
-        } catch (err) {
-            console.error('Error getting tab URL:', err);
-        }
-    }
-
-    (async () => {
-        let url = await getCurrentTabUrl();
-
-        while (!url) {
-            setInterval(() => {}, 1000);
-            url = await getCurrentTabUrl();
-        }
-
-        console.log('Current tab URL:', url);
-    })();
-
     browser.runtime.onMessage.addListener((message, _, sendResponse) => {
         console.log('Message from:', message);
 
