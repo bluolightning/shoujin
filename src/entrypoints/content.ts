b39e2a1899882db07e5b85179f477e803edc4869
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-03-01 21:42:50 -0500

    Add idle state tracking

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -7,6 +7,7 @@ export default defineContentScript({
     main() {
         let startTime = new Date();
         let endTime: Date;
+        let sessionStatus = true;
 
         const urlInfo = {
             href: window.location.href,
@@ -18,7 +19,7 @@ export default defineContentScript({
         const sendMessage = async (
             type: 'page-focused' | 'page-unfocused' | 'detect-idle',
             time?: number | null | undefined
-        ): Promise<void> => {
+        ): Promise<string> => {
             const response: MessageResponse = await browser.runtime.sendMessage(
                 {
                     type,
@@ -29,24 +30,67 @@ export default defineContentScript({
             );
 
             console.log('logging response:', response.status);
+            return response.status;
         };
+
+        function endSession() {
+            if (!sessionStatus) {
+                throw new Error('Session already ended');
+            }
+            endTime = new Date();
+            const timeOnPage = (+endTime - +startTime) / 1000;
+            const roundedTime = Math.round(timeOnPage * 1000) / 1000;
+            console.log(roundedTime);
+
+            sessionStatus = false;
+
+            sendMessage('page-unfocused', roundedTime);
+        }
+
+        function startSession() {
+            if (sessionStatus) {
+                throw new Error('Session already in progress');
+            }
+            sessionStatus = true;
+            startTime = new Date();
+            sendMessage('page-focused', null);
+        }
 
         // Listen for page visibility changes
         document.addEventListener('visibilitychange', function () {
             if (document.hidden) {
-                endTime = new Date();
-                const timeOnPage = (+endTime - +startTime) / 1000;
-                const roundedTime = Math.round(timeOnPage * 1000) / 1000;
-                console.log(roundedTime);
-
-                sendMessage('page-unfocused', roundedTime);
+                endSession();
             } else {
-                startTime = new Date();
-                sendMessage('page-focused', null);
+                startSession();
             }
         });
 
-        console.log('detecting idle...');
-        sendMessage('detect-idle', null);
+        // Detect idle state - may check computer activity, rather than browser activity
+        let count = 0;
+        const intervalId = setInterval(async () => {
+            console.log('detecting idle...');
+            const message = await sendMessage('detect-idle', null);
+
+            if (message === 'active') {
+                console.log('active');
+                if (!sessionStatus) {
+                    startSession();
+                }
+            } else if (message === 'idle' || message === 'locked') {
+                console.log('idle or locked');
+                if (sessionStatus) {
+                    endSession();
+                }
+            } else {
+                console.log('invalid idle state');
+            }
+
+            count++;
+            if (count === 240) {
+                // 240 * 15s = 1 hour; the interval will then be cleared
+                clearInterval(intervalId);
+                console.log('interval cleared');
+            }
+        }, 15000);
     },
 });
