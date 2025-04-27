47d5343b281c2059bf646225a3687e82460d4fd2
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-04-27 17:35:15 -0400

    Add more info to errors when favicons are failed to be fetched

--- src/utils/getFavicon.ts
+++ src/utils/getFavicon.ts
@@ -7,9 +7,10 @@ export default async function getFavicon(url: string) {
         if (tabs[0].favIconUrl) {
             return tabs[0].favIconUrl;
         } else {
+            console.log('No favicon found, using Google favicon service.');
             return `https://www.google.com/s2/favicons?domain=${url}&sz=128`;
         }
     } catch (err: unknown) {
-        console.error('Error getting tab URL:', err);
+        console.error('Error getting tab URL:', err, "\n errored site info:", url);
     }
 }
