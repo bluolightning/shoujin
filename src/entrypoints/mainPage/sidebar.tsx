be3cbfc145b54944b80412b54530904fa1a9c673
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-06-10 13:38:50 -0400

    Remove shadcn components from the page wrapper

--- src/entrypoints/mainPage/sidebar.tsx
+++ src/entrypoints/mainPage/sidebar.tsx
@@ -1,63 +1,21 @@
 'use client';
 import React from 'react';
+import { AppShell, MantineProvider } from '@mantine/core';
 import { AppSidebar } from '../../components/appSidebar';
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from '@/components/ui/sidebar';
 import { Outlet } from 'react-router';
-import { ThemeProvider } from '@/components/theme-provider';
-import { ModeToggle } from '@/components/mode-toggle';
 
 export default function Page() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                        {/* Header stuff */}
-                        <div className="flex items-center gap-2 px-3">
-                            <SidebarTrigger />
-                            <Separator
-                                orientation="vertical"
-                                className="mr-2 h-4"
-                            />
-                            <Breadcrumb>
-                                <BreadcrumbList>
-                                    <BreadcrumbItem className="hidden md:block">
-                                        <BreadcrumbLink>App</BreadcrumbLink>
-                                    </BreadcrumbItem>
-                                    <BreadcrumbSeparator className="hidden md:block" />
-                                    <BreadcrumbItem>
-                                        <BreadcrumbPage>
-                                            breadcrumb
-                                        </BreadcrumbPage>
-                                    </BreadcrumbItem>
-                                </BreadcrumbList>
-                            </Breadcrumb>
-                            <Separator
-                                orientation="vertical"
-                                className="mr-2 h-4"
-                            />
-                            <ModeToggle />
-                        </div>
-                    </header>
-                    <div className="flex flex-1 flex-col gap-4 p-4">
-                        <Outlet />
-                    </div>
-                </SidebarInset>
-            </SidebarProvider>
-        </ThemeProvider>
+        <MantineProvider defaultColorScheme="dark">
+            <AppShell navbar={{ width: 300, breakpoint: 'sm' }} padding="md">
+                <AppShell.Navbar>
+                    <AppSidebar />
+                </AppShell.Navbar>
+
+                <AppShell.Main>
+                    <Outlet />
+                </AppShell.Main>
+            </AppShell>
+        </MantineProvider>
     );
 }
