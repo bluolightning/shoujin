7a5423e2107afd233d70fee64ad6f8944d2a4b92
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-06-20 13:19:19 -0400

    Remove the old pie chart

--- src/entrypoints/mainPage/Dashboard.tsx
+++ src/entrypoints/mainPage/Dashboard.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import {Grid} from '@mantine/core';
-import {PieChartDonut} from '@/components/PieChartDonut';
 import SiteUsageList from '@/components/SiteUsageList';
 import PieChartDonutNew from '@/components/PieChartDonutNew';
 
 const Dashboard = () => {
     return (
         <Grid gutter='md'>
-            <Grid.Col span={{base: 12, md: 5, lg: 4}}>
-                <PieChartDonut />
-            </Grid.Col>
             <Grid.Col span={{base: 12, md: 5, lg: 4}}>
                 <div>
                     <PieChartDonutNew />
