28fa4a78431cd004bbbc4635b8f6efe03ce14ae7
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-06-22 15:32:16 -0400

    Fix wxt dev server - have it no longer use Chrome For Testing binary

--- web-ext.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineWebExtConfig } from 'wxt';
-
-export default defineWebExtConfig({
-    binaries: {
-        chrome: 'C:/Users/Public/chrome-win64/chrome.exe', // Use the path to Chrome for Testing (download at https://googlechromelabs.github.io/chrome-for-testing/)
-    },
-});
