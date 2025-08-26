b71ea014ee9e0402bc79ca1c4b3bb3a7f22ad549
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-08-26 00:52:24 -0400

    Add a header to the dashboard

--- src/entrypoints/MainPage/Dashboard.tsx
+++ src/entrypoints/MainPage/Dashboard.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {Grid} from '@mantine/core';
+import {Grid, Group, Text} from '@mantine/core';
 import SiteUsageList from '@/components/SiteUsageList';
 import PieChartDonut from '@/components/PieChartDonut';
 import DateSelector from '@/components/DateSelector';
@@ -18,6 +18,21 @@ export default function Dashboard() {
 
     return (
         <dateRangeContext.Provider value={[dateRange, setDateRange]}>
+            <Group
+                justify='space-between'
+                style={{
+                    paddingBottom: 'var(--mantine-spacing-md)',
+                    marginBottom: 'calc(var(--mantine-spacing-md) * 1.5)',
+                    borderBottom:
+                        '1px solid light-dark(var(--mantine-color-gray-3), var(--mantine-color-dark-4))',
+                }}>
+                <div>
+                    <Text size='lg' fw={700}>
+                        Header
+                    </Text>
+                </div>
+            </Group>
+
             {/* Allow dateRange to be passed to children */}
             <Grid gutter='md'>
                 <Grid.Col span={{base: 12, md: 5, lg: 4}}>
