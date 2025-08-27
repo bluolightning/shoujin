a8ea0a8c24f301f772b52b61f999fce52b4bc228
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-08-26 23:35:15 -0400

    Add helper function to sort time data by date

---
+++ src/utils/getTimeByDate.ts
@@ -0,0 +1,13 @@
+import {PageTimeEntry} from './storage';
+
+export type TimeByDateMap = {[key: string]: number};
+
+export function getTimeByDate(data: PageTimeEntry[]): TimeByDateMap {
+    return data.reduce((acc, site) => {
+        for (const [date, {dailyTime}] of Object.entries(site.dateData)) {
+            const roundedTime = Math.round(dailyTime);
+            acc[date] = (acc[date] || 0) + roundedTime;
+        }
+        return acc;
+    }, {} as TimeByDateMap);
+}
