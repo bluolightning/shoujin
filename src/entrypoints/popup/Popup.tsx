e1b523b41eec9542000827920b86e84be32ab2cf
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-09-02 20:10:37 -0400

    Remove unnecessary url on popup

--- src/entrypoints/popup/Popup.tsx
+++ src/entrypoints/popup/Popup.tsx
@@ -1,27 +1,6 @@
-import {useState, useEffect} from 'react';
 import './Popup.css';
 
 export default function Popup() {
-    const [currentUrl, setCurrentUrl] = useState<string | null>(null);
-
-    useEffect(() => {
-        const getCurrentTabUrl = async () => {
-            try {
-                const tabs = await browser.tabs.query({
-                    active: true,
-                    currentWindow: true,
-                });
-                if (tabs && tabs[0] && tabs[0].url) {
-                    setCurrentUrl(tabs[0].url);
-                }
-            } catch (err: unknown) {
-                console.error('Error getting tab URL:', err);
-            }
-        };
-
-        getCurrentTabUrl();
-    }, []);
-
     const openNewTab = () => {
         browser.tabs.create({
             url: browser.runtime.getURL('/MainPage.html'),
@@ -30,7 +9,6 @@ export default function Popup() {
 
     return (
         <>
-            <h2>{currentUrl}</h2>
             <button onClick={openNewTab}>Open Extension Page</button>
         </>
     );
