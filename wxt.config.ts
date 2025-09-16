e57342732128a6a97d369ed96dbffadc42f222c0
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-09-16 07:35:45 -0400

    Get a working custom app icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     browser: 'chrome',
-    modules: ['@wxt-dev/module-react'],
+    modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
     srcDir: 'src',
 
     manifest: {
