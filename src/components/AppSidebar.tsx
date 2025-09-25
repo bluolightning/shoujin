c8b769cc034a9778a50f22a156e1594567f1c785
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-09-25 15:52:37 -0400

    Bump version to 1.0.0

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -46,7 +46,7 @@ export default function AppSidebar() {
                             shoujin
                         </Text>
                     </div>
-                    <Code fw={700}>v0.0.1</Code>
+                    <Code fw={700}>v1.0.0</Code>
                 </Group>
                 {links}
             </div>
